c2f68e6cd28598ae906588f1778a6dbf11e986f4
John <john.zhao@scrumcn.com>
2021-10-05 11:20:50 +0800

    优化

--- lib/live_board_web/components/card_modal/card_desc.ex
+++ lib/live_board_web/components/card_modal/card_desc.ex
@@ -23,11 +23,11 @@ defmodule CardDesc do
           <span x-on:click="CardDescHook.initEditEditor();is_show_button=true" class="text-gray-400 text-sm">编辑</span>
         </a>
       </div>
-      <input type="hidden" id="card-desk-info-value" value={@card_desc_info} />
-      <div id="md-editor" class="pl-4">{@card_desc_info}</div>
+      <input type="hidden" id="card-desc-info-value" value={@card_desc_info} />
+      <div id="md-editor" class="pl-4"></div>
       <template x-if="is_show_button">
         <div class="flex flex-row items-center ml-4">
-          <button x-on:click="CardDescHook.saveContent" class="w-20 h-8 mt-2 bg-[rgb(0,121,191)] hover:bg-[rgb(2,106,167)] rounded text-white">保存</button>
+          <button x-on:click="CardDescHook.saveContent();is_show_button=false" class="w-20 h-8 mt-2 bg-[rgb(0,121,191)] hover:bg-[rgb(2,106,167)] rounded text-white">保存</button>
           <button x-on:click="CardDescHook.initViewEditor();is_show_button=false" class="w-20 h-8 mt-2 ml-2 bg-white hover:bg-gray-300 border rounded">取消</button>
         </div>
       </template>
